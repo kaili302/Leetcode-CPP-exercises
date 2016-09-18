64bd8e6df330d7b5eb409b3c6662e49d567e2dd9
snail914 <kai_li@outlook.com>
2016-09-18 21:43:30 +0200

    nim game

--- 11. Others/3) Nim Game.cpp
+++ 11. Others/3) Nim Game.cpp
@@ -9,4 +9,6 @@ For example, if there are 4 stones in the heap, then you will never win the game
 no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.
 */
 
+return n % 4;
+
 
